a9764a35ba73caa60320f12b165fc33691b39262
cold14022000 <85611457+cold14022000@users.noreply.github.com>
2021-11-07 23:07:17 +0700

    lab3

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => { 
+
+        res.send('Hello World, from express'); 
+    
+}); 
+
+const PRODUCTS = [ 
+
+      { 
+    
+        imageLink: "http://mvbinz.xyz/images/1.jpg", 
+    
+        name: "Rayon a-line Dress", 
+    
+        price: "13.45", 
+    
+        type: "Other", 
+    
+      }, 
+    
+    ]; 
+ app.post("/products", (req, res) => { 
+
+          const product = req.body; 
+        
+         
+        
+          // Output the product to the console for debugging 
+        
+          console.log(product); 
+        
+          PRODUCTS.push({...product}); 
+        
+         
+        
+          res.status(201).json({mess: "Products is added to the database"}); 
+        
+        }); 
+
+app.get("/products", (req, res) => { 
+      res.status(200).json({ products: PRODUCTS }); 
+ }); 
+
+app.listen(PORT, () => console.log(`Hello world app listening on port ${PORT} with url http://localhost:${PORT}`) 
+); 
